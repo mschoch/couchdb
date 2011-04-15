08b1a445be554214aaea906e45285fe96eb1dd10
Filipe David Manana <fdmanana@apache.org>
2011-04-15 19:37:26 +0100

    Build doc summary chunks in a single place

--- src/couchdb/couch_util.erl
+++ src/couchdb/couch_util.erl
@@ -497,4 +497,7 @@ decompress(<<?TERM_PREFIX, _/binary>> = Bin) ->
 is_compressed(<<?SNAPPY_PREFIX, _/binary>>) ->
     true;
 is_compressed(<<?TERM_PREFIX, _/binary>>) ->
+    true;
+is_compressed(_) ->
     false.
+
