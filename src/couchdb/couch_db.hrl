17754ef7e5bebaf228e4bb46a38ebed0f4e257a6
Filipe David Borba Manana <fdmanana@apache.org>
2011-05-31 10:04:20 +0000

    More efficient term size calculation
    Unlike byte_size(term_to_binary(Term)), the BIF erlang:external_size/1 doesn't
    do the serialization step, it only calculates the maximum external size for
    any term, which is more efficient (faster and avoids the garbage generation).
    
    With the test couch_http_bulk_writes.sh at [1], using 20 writers and batches
    of 100 1Kb documents, it's possible to write about 1 400 000 documents with
    this patch instead of about 1 300 000.
    
    [1] https://github.com/fdmanana/basho_bench_couch
    
    Change-Id: I7cbfe1a97c6247ac0188019d31af7603ff11a3da
    git-svn-id: https://svn.apache.org/repos/asf/couchdb/trunk@1129597 13f79535-47bb-0310-9956-ffa450edef68
    Reviewed-on: http://review.couchbase.org/6689
    Reviewed-by: Filipe David Borba Manana <fdmanana@gmail.com>
    Tested-by: Filipe David Borba Manana <fdmanana@gmail.com>
    Reviewed-by: Jan Lehnardt <jan@apache.org>
    Tested-by: Jan Lehnardt <jan@apache.org>

--- src/couchdb/couch_db.hrl
+++ src/couchdb/couch_db.hrl
@@ -28,6 +28,12 @@
 -define(l2b(V), list_to_binary(V)).
 -define(l2i(V), list_to_integer(V)).
 -define(term_to_bin(T), term_to_binary(T, [{minor_version, 1}])).
+-define(term_size(T),
+    try
+        erlang:external_size(T)
+    catch _:_ ->
+        byte_size(?term_to_bin(T))
+    end).
 
 -define(DEFAULT_ATTACHMENT_CONTENT_TYPE, <<"application/octet-stream">>).
 
